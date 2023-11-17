e97976d3bc48ba0326b2d1cff01f429442014b06
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-17 13:39:53 -0300

    Motocycle.cs

---
+++ Exercises/Inheritance/Motocycle.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Library
+{
+    internal class Motocycle : Vehicle //class Motocycle comes from class  vehicle base
+    {
+        public int rodas;
+    }
+}
