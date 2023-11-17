641ec772c9867bd5abc9586e276b5a47b6f4245c
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-17 13:38:39 -0300

    Vehicle.cs

---
+++ Exercises/Inheritance/Vehicle.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Library
+{
+    internal class Vehicle
+    {
+        public string branch;
+        public string model;
+        public int AmoutnPeople;
+        public int Year;
+
+        public void Move()
+        {
+
+        }
+
+    }
+}
