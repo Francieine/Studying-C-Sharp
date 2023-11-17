a1ebcdbe7148d16422ca5a12b52309891569894a
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-17 13:39:21 -0300

    Car.cs

---
+++ Exercises/Inheritance/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Library
+{
+    internal class Car : Vehicle //class car comes from class vehicle base
+    {
+        public int rodas;
+    }
+}
