9bef2a5824153def7d73629695c5e5458e572eaf
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-24 11:22:13 -0300

    Pessoa.cs

---
+++ Exercises/Partials/Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    partial class Pessoa
+    {
+        partial void FacaAlgo(string andar) //implementação do metodo
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
