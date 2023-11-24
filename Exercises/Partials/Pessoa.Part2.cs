45c7888e7b1467807e93808aaa07d17785b21604
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-24 11:23:06 -0300

    Pessoa.Part2.cs

---
+++ Exercises/Partials/Pessoa.Part2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    partial class Pessoa
+    {
+        partial void FacaAlgo(String andar); //declaração do metodo
+        
+        //Assinatua do metodo parcial. Obs.: metodo parcital, não pode ser publico
+    }
+}
