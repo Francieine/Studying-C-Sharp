588f7e83fa7ff682cd22b77634b3303c4995baa5
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-27 09:06:46 -0300

    Selead.cs

---
+++ Exercises/Selead.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selead.Lib
+{
+    sealed class Veiculo // a classe selada não pode ter outras classes derivadas dela
+    {
+      
+    }
+}
