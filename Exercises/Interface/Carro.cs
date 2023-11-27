510f48e458bb1139b619ee24e605cae7be4f2c56
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-27 09:40:15 -0300

    Carro.cs

---
+++ Exercises/Interface/Carro.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.Lib
+{
+    class Carro : Veiculo, ITransporte
+    {
+        public override string Marca { get => throw new NotImplementedException(); set => throw new NotImplementedException(); } //implementação set e get herdada da classe veiculo
+
+        public override string Modelo => throw new NotImplementedException(); //implementação get herdada da classe veiculo
+
+        public void Mover(byte velocidade) //implementação da Interface
+        {
+            Console.WriteLine("aqui está ação mover");
+        }
+
+        public override void MudarMarcha(byte NumeroMarcha)
+        {
+            throw new NotImplementedException(); //cada classe terá sua implementação de metodo, caso as outras classes tenham ações diferentes
+        }
+    }
+}
