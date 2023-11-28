8df3728f9304fc0a56a01cc5f50fae956266b289
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-28 10:50:18 -0300

    Veiculo2.cs

---
+++ Exercises/Base/Veiculo2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+     class Veiculo2
+    {
+    public virtual void Andar() //metodo
+        {
+            Console.WriteLine("Veiculo Andando");
+        }
+    }
+}
