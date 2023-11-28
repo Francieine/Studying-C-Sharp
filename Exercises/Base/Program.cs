1b7e7df57ee84bbb6b671ea047048f1dedbf1c2f
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-28 10:51:16 -0300

    Program.cs

---
+++ Exercises/Base/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo2 veiculo = new Veiculo2();
+            veiculo.Andar();
+
+            Console.ReadKey();
+        
+        }
+        }
+}
