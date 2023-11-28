9b2c43d574838b9e73466d1421534848e8ccad86
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-11-28 11:20:58 -0300

    Program.cs

---
+++ Exercises/Base-Constructor/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro2 carro = new Carro2(19);
+            
+
+            Console.ReadKey();
+        
+        }
+        }
+}
